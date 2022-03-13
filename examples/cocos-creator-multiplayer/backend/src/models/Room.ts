b4b6ca873a1d00bd0d648bcc11e52d9f58b155a5
liupu <liupu@fotoable.com>
2022-03-13 12:55:33 +0800

    服务器房间注释

--- examples/cocos-creator-multiplayer/backend/src/models/Room.ts
+++ examples/cocos-creator-multiplayer/backend/src/models/Room.ts
@@ -11,18 +11,26 @@ export class Room {
 
     // 帧同步频率，次数/秒
     syncRate = gameConfig.syncRate;
+    // 玩家 ID 生成器
     nextPlayerId = 1;
 
+    // 游戏状态
     gameSystem = new GameSystem();
 
+    // 基于 WebSocket 的服务器
     server: WsServer<ServiceType>;
+    // WebSocket 连接
     conns: WsConnection<ServiceType>[] = [];
+    // 待处理的输入
     pendingInputs: GameSystemInput[] = [];
+    // 玩家最近一个确认号
     playerLastSn: { [playerId: number]: number | undefined } = {};
+    // 最近一次同步时间
     lastSyncTime?: number;
 
     constructor(server: WsServer<ServiceType>) {
         this.server = server;
+        // 设置处理速度
         setInterval(() => { this.sync() }, 1000 / this.syncRate);
     }
 
@@ -37,10 +45,15 @@ export class Room {
                 y: Math.random() * 10 - 5
             }
         }
+        // 加入待处理消息队列
         this.applyInput(input);
 
+        // 将连接加入连接队列
         this.conns.push(conn);
+
+        // 绑定连接与玩家
         conn.playerId = input.playerId;
+        
         conn.listenMsg('client/ClientInput', call => {
             this.playerLastSn[input.playerId] = call.msg.sn;
             call.msg.inputs.forEach(v => {
@@ -58,10 +71,15 @@ export class Room {
         this.pendingInputs.push(input);
     }
 
+    /**
+     * 服务器 LOCKSTEP 帧同步广播
+     */
     sync() {
+        // 取出所有待处理的消息
         let inputs = this.pendingInputs;
         this.pendingInputs = [];
 
+        // 修改游戏状态
         // Apply inputs
         inputs.forEach(v => {
             this.gameSystem.applyInput(v)
@@ -69,11 +87,13 @@ export class Room {
 
         // Apply TimePast
         let now = process.uptime() * 1000;
+        // 将时间流逝封装成事件
         this.applyInput({
             type: 'TimePast',
             dt: now - (this.lastSyncTime ?? now)
         });
         this.lastSyncTime = now;
+        //console.log("server sync time:" + now);
 
         // 发送同步帧
         this.conns.forEach(v => {
@@ -86,7 +106,10 @@ export class Room {
 
     /** 离开房间 */
     leave(playerId: number, conn: WsConnection<ServiceType>) {
+        // 删除连接
         this.conns.removeOne(v => v.playerId === playerId);
+        
+        // 加入消息
         this.applyInput({
             type: 'PlayerLeave',
             playerId: playerId
